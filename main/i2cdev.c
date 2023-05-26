76503a6f4bc0997caf071ea5cc4a171a03623c37
Vaibhav <vaibhav@yantrr.com>
2023-05-26 14:18:28 +0530

    changes for yantrr's omny-s3

--- main/i2cdev.c
+++ main/i2cdev.c
@@ -15,8 +15,8 @@ esp_err_t i2c_master_init(i2c_port_t port, int sda, int scl)
 {
         i2c_config_t i2c_config = {
                 .mode = I2C_MODE_MASTER,
-                .sda_io_num = sda,
-                .scl_io_num = scl,
+                .sda_io_num = 14,
+                .scl_io_num = 21,
                 .sda_pullup_en = GPIO_PULLUP_ENABLE,
                 .scl_pullup_en = GPIO_PULLUP_ENABLE,
                 .master.clk_speed = 1000000
